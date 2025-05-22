ff36e921478eb6ce390d1ec07a9a68a3e7a9507f
unclethoby <105172401+unclethoby@users.noreply.github.com>
2025-05-22 11:38:56 +0100

    Integrate authentication with swagger ui

--- Login_project/settings.py
+++ Login_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'login',
     'drf_yasg'
+    
 ]
 
 MIDDLEWARE = [
--- Login_project/urls.py
+++ Login_project/urls.py
@@ -39,4 +39,28 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Login API",
+        default_version='v1',
+        description="API documentation for JWT Auth system (register, login, reset password, etc.)",
+        contact=openapi.Contact(email="support@example.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('login.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+]
